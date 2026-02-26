5b817a2fa5c98d08b19d875aa2f71e5ab24bdd37
danie <calzada.dsg@gmail.com>
2026-02-26 09:53:55 -0600

    Migracion

---
+++ TestNuxibaAPI/Migrations/20260226155201_InitialCreate.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestNuxibaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ccRIACat_Areas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IDArea = table.Column<int>(type: "int", nullable: false),
+                    AreaName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    StatusArea = table.Column<int>(type: "int", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ccRIACat_Areas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ccUsers",
+                columns: table => new
+                {
+                    User_id = table.Column<int>(type: "int", nullable: false),
+                    Login = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Nombres = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ApellidoPaterno = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ApellidoMaterno = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    TipoUser_id = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    fCreate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IDArea = table.Column<int>(type: "int", nullable: false),
+                    LastLoginAttempt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ccUsers", x => x.User_id);
+                    table.ForeignKey(
+                        name: "FK_ccUsers_ccRIACat_Areas_IDArea",
+                        column: x => x.IDArea,
+                        principalTable: "ccRIACat_Areas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ccloglogin",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    User_id = table.Column<int>(type: "int", nullable: false),
+                    Extension = table.Column<int>(type: "int", nullable: false),
+                    TipoMov = table.Column<int>(type: "int", nullable: false),
+                    fecha = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ccloglogin", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ccloglogin_ccUsers_User_id",
+                        column: x => x.User_id,
+                        principalTable: "ccUsers",
+                        principalColumn: "User_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ccloglogin_User_id",
+                table: "ccloglogin",
+                column: "User_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ccUsers_IDArea",
+                table: "ccUsers",
+                column: "IDArea");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ccloglogin");
+
+            migrationBuilder.DropTable(
+                name: "ccUsers");
+
+            migrationBuilder.DropTable(
+                name: "ccRIACat_Areas");
+        }
+    }
+}
